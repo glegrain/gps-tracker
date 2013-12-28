4c172c910b2419ee671c9d8d74288862831e16ae
Guillaume Legrain <legrainguillaume@gmail.com>
2013-12-28 17:26:32 +0100

    Fixed fatabase url

--- routes/devices.js
+++ routes/devices.js
@@ -24,8 +24,8 @@
 // });
 
 var pg = require('pg');
-var conString = process.env.DATABASE_URL || 'postgres://localhost:5432/gpstracker'; // heroku defines DATABASE_URL
-//var conString = process.env.DATABASE_URL || 'postgres://prszbsgoyrzzak:vow4DCek_eqiUfFMiQpa9Pj4y-@ec2-54-228-227-194.eu-west-1.compute.amazonaws.com:5432/d8hg0ujmn2vs6e';
+var conString = HEROKU_POSTGRESQL_COPPER_URL || 'postgres://localhost:5432/gpstracker'; // heroku defines DATABASE_URL
+//var conString = process.env.HEROKU_POSTGRESQL_COPPER_URL || 'postgres://prszbsgoyrzzak:vow4DCek_eqiUfFMiQpa9Pj4y-@ec2-54-228-227-194.eu-west-1.compute.amazonaws.com:5432/d8hg0ujmn2vs6e';
 console.log(conString);
 var client = new pg.Client(conString);
 client.connect(function(err) {
